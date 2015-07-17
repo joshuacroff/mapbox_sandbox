d71f85fbec5e80e4582a0d22c472a56e1965a9ae
joshuacroff <joshua.croff@gmail.com>
2015-07-17 16:52:14 -0700

    Change format of object sent from sever to frontend

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,7 @@ class HomeController < ApplicationController
     def sfdata
       @geojson = HTTParty.get("https://data.sfgov.org/resource/qg52-sqku?$$app_token=Tfqfz8OmwAEF8YU3FYwmm2xaD")
 
-      render json: {geojson: @geojson}
+      render json: @geojson
     end
 
 end
